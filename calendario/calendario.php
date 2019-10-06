05773f395c14990bb99693e2f1411116d95b133c
MateoCE <48710211+MateoCE@users.noreply.github.com>
2019-10-06 17:17:24 +0200

    Update calendario
    Solucionado el día de inicio del mes

--- calendario/calendario.php
+++ calendario/calendario.php
@@ -12,32 +12,31 @@
 		$daysOfTheMonth = date("t");
 		$dias = 1;
 		$dia =1;
-
-		echo date('Y-m-01', strtotime($query_date));
+		$month_start = strtotime('first day of this month', time());
+		$firstDayOfTheMonth = date('w', $month_start);
 		
-		echo "<table border=1 width=294>";
+		echo "<table border=1 width=294 >";
 	 	echo "<tr><th colspan=7> $nameMonth $year </th></tr>";
 	 	echo "<tr><td width=42>Lunes</td>	<td width=42>Martes</td>	<td width=42>Miercoles</td>		<td width=42>Jueves</td>
 			  <td width=42>Viernes</td>		<td width=42>Sabado</td>	<td width=42>Domingo</td></tr>";
-
-
-
 		for ($contadorSemana=0; $contadorSemana <5; $contadorSemana++) { 
 			# code...
 			echo "<tr></tr>";
 			
 			for ($contadorDia=0; $contadorDia <7 ; $contadorDia++) { 
-				if($dias<$dayOfTheWeek || $dia>$daysOfTheMonth){
+				if($dias<$firstDayOfTheMonth|| $dia>$daysOfTheMonth){
 					echo "<td> </td>";
+				}else if($dia==$day){
+					echo "<td bgcolor='cyan' align=center> $dia </td>";
+					$dia +=1;
 				}else{
-					echo "<td> $dia </td>";
+					echo "<td align=center> $dia </td>";
 					$dia +=1;
 				}
 				$dias +=1;
 			}
 			
 		}
-
 	?>
 
 </head>
